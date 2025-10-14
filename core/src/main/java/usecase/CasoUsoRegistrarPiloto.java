522365f8f9d46bdaaad28cc04bb5b3696afcda25
RoAntoo <rochiramaccioni@gmail.com>
2025-10-14 17:39:43 -0300

    Se agrego la clase caseUse:RegistrarPiloto

--- core/src/main/java/usecase/CasoUsoRegistrarPiloto.java
+++ core/src/main/java/usecase/CasoUsoRegistrarPiloto.java
@@ -1,7 +1,36 @@
 package usecase;
 
-public class CasoUsoRegistrarPiloto {
+import java.time.LocalDate;
+import java.util.UUID;
 
+import exception.ExceptionPiloto;
+import input.IRegistroPiloto;
+import model.Piloto;
+import output.IRepositorioPiloto;
+
+public class CasoUsoRegistrarPiloto implements IRegistroPiloto{
+	private final IRepositorioPiloto rep;
+
+	public CasoUsoRegistrarPiloto(IRepositorioPiloto rep) {
+		this.rep = rep;
+	}
+	
+	@Override
+	public UUID registrarPiloto(UUID id, String nombre, String documento, LocalDate fecha_Nac) throws ExceptionPiloto {
+		Piloto piloto = Piloto.factory(id, nombre, documento, fecha_Nac);
+		
+		if (rep.validarPiloto(piloto.getDocumento())) {
+			throw new ExceptionPiloto("ERROR: El piloto que intenta registrar ya existe cargado");
+		}
+		
+		if (!rep.guardarPiloto(piloto)) {
+			throw new ExceptionPiloto("ERROR: No se pudo guardar al piloto");
+		}
+		
+		return piloto.getId();
+	}
+	
+	
 	
 	
 }
